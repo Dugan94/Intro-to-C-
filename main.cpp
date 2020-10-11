f001b1c90c744ca9f72dbc553b4691c3c6231e40
Brandon Dugan <72161175+Dugan94@users.noreply.github.com>
2020-10-10 19:44:59 -0700

    Add files via upload
    Twitter Analyzer

--- main.cpp
+++ main.cpp
@@ -1,95 +1,109 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
 using namespace std;
 
-int main()
-
+int main() 
 {
-  char corroded; //Silent when Key is turned, Y or N
-  char clicking; // Not silent when key is turned, Y or No
-  char cranking; // When car fails to start
-  char silent; // No sound
-  char noise; // sound
+    string largestTwt;
+    string fname; 
+    string tmp;
+    ifstream inFile;
+    ifstream Input;
+    ofstream Output;
+    int longestLine = 0;      // The longest line so far in the file
+    int lineCount = 0;        // Number of tweets (lines) in the file
+    bool openFailed = false;  // Assume success
+    int IDcount = 0; 
+    int Hashtags = 0;
+    int currentSize = 0;
+    int lengthTotal = 0; 
+    int largestLength = 0; 
+    int largestTweet = 0; 
+    int largestHash = 0;
+   
+    //inFile.open("allTweets.txt");
 
-  cout << "Answer the following questions\n";
-  cout << "with either Y for Yes or ";
-  cout << "N or No.\n";
-  cout << "Is the car silent when you turn the key?\n";
-  cin >> silent;
-  cout << "Are the battery terminals corroded?\n";
-  cin >> corroded;
-  cout << "Does the car make a clicking noise?\n";
-  cin >> clicking;
-  cout << "Does the car crank up but fail to start?\n";
-  cin >> cranking;
+    // This do-while loop will set a boolean variable 'openFailed'
+    // if the user gives us a bad filename, then continue to ask again.
+    do {
+        cout << "Filename to open?";
 
-  // Determine the battery terminals
-  if (corroded == 'Y')
-  {
-    cout << "Clean the terminals and try starting again.\n";
-  }
-else // Batteries corroded
-{
-  cout << "Replace the cables and try again.\n";
-}
-// Determine if the car makes a clicking sound
-if (clicking == 'Y')
-{
-  cout << "Replace the battery.\n";
-}
-else // Not clicking
-{
-  cout << " Does the car crank up but fail to start?\n";
-}
-if (cranking == 'Y')
-{
-  cout << "Check the spark plug connections.\n";
-}
-else // fail to start
-{
-  cout << "Take car in for service.\n";
-}
-return 0;
-}
+        // Wait for input of the filename
+        getline(cin, fname);
 
+        cout << "The filename is:" << fname << endl;
+        cout << "Analysis for file allTweets.txt:" << endl;
 
+        // Open the file
+        inFile.open(fname.c_str());
 
+        if (inFile.fail()) {
+            cout << "Invalid filename, try again." << endl;
+            openFailed = true;
+        } 
+    } while (openFailed);
 
+       
+        // The getline() inside the while() will
+        // return FALSE when the end of the
+        // file is reached.
+        while (getline(inFile, tmp))
+        {
+         lineCount++;
+          
+        int c = count(tmp.begin(), tmp.end(), '@');
+        IDcount += c;
+        int d = count(tmp.begin(), tmp.end(), '#');
+        Hashtags += d; 
+        cout << tmp << endl;
+            cout << tmp << " : length " << tmp.length()
+                    << endl; 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //Length
+        int currentSize = tmp.size();
+        lengthTotal += currentSize; 
+        if (currentSize >= largestLength){
+            largestLength = currentSize;
+            largestTweet = lineCount;
+            largestHash = Hashtags;
+            largestTwt = tmp; 
+        
+        }
+        
+            // Output the results
+        cout << "Longest tweet: " << largestLength << endl; 
+        cout << "Total number of tweets: " << largestTweet << endl;
+        cout << "Twitter IDs: " << IDcount << endl;
+        cout << "Twitter Hashtag: " << Hashtags << endl;
+        cout << "Largest Tweet: " << largestTwt << endl;
+        cout << "Hashtags in longest Tweet: " << largestHash << endl;
+        cout << "Total Tweets: " << lineCount << endl; }
+        
+    
+        // Then close the file.
+        inFile.close();
+       
+        return 0;
 
 
+} 
+ 
+
+
+
+     
 
 
 
 
+
+       
+ 
+
+
+        
+        
 
